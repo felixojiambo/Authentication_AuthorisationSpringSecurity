4c90a40e4879fc9be47aa3be9e612824fc52e02c
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-04 22:37:08 -0700

    setup

---
+++ src/main/java/com/ecom/Authorisation_Authentication/AuthenticationAuthorisationApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.Authorisation_Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAuthorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAuthorisationApplication.class, args);
+		System.out.println("User Authorisation");
+	}
+
+}
