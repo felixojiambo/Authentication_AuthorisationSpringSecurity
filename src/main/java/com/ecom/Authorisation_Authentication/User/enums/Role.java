442f49501224e2f9b25b9b5928f957228177c8ad
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-09 02:40:37 -0700

    make role value final

--- src/main/java/com/ecom/Authorisation_Authentication/User/enums/Role.java
+++ src/main/java/com/ecom/Authorisation_Authentication/User/enums/Role.java
@@ -1,17 +1,16 @@
 package com.ecom.Authorisation_Authentication.User.enums;
-
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
+@Getter
 public enum Role implements GrantedAuthority {
     ROLE_USER("USER"),
     ROLE_ADMIN("ADMIN");
-    private String value;
+    private final String value;
     Role(String value) {
         this.value = value;
     }
-    public String getValue() {
-        return this.value;
-    }
+
     @Override
     public String getAuthority() {
         return name();
